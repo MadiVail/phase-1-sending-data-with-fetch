f33d93f2155841fc7134826b778c1e08cd1c39d4
Madisyn Vail <virtualmadi3@gmail.com>
2022-01-31 15:16:38 -0600

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    const subData = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        }),
+    };
+
+    return fetch("http://localhost:3000/users", subData)
+    .then((response) => response.json())
+    .then((body) => addNewContact(body.id))
+
+    .catch((err) => addNewContact(err.message))
+
+};
+
+function addNewContact (newID) {
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+}
+
+submitData("name", "name@name.com")
